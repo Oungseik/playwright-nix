632dddfcf70ab7f1c34155c0d4ce8110adad6c87
Oung Seik <mhemaungthuwin@gmail.com>
2025-12-12 09:17:23 +0630

    chore: update playwright to latest

--- flake.nix
+++ flake.nix
@@ -2,26 +2,36 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
+    playwright.url = "github:pietdevries94/playwright-web-flake";
   };
   outputs =
     {
       self,
       nixpkgs,
       utils,
+      playwright,
     }:
     utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        overlay = final: prev: {
+          inherit (playwright.packages.${system}) playwright-test playwright-driver;
+        };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+        };
       in
       {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             nodejs_24
+            pkgs.playwright-test
           ];
 
           LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
-          PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright.browsers}";
+          PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = 1;
+          PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
           PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = true;
           PLAYWRIGHT_NODEJS_PATH = "${pkgs.nodejs_24}/bin/node";
           PLAYWRIGHT_HOST_PLATFORM_OVERRIDE = "";
